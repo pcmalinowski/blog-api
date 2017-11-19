863a27ee35969a9c24d5aeffd9cd9823ff13e37d
pcmalinowski <pcmalinowski@gmail.com>
2017-11-19 00:37:51 -0600

    fin

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require(`express`);
 const morgan = require(`morgan`)
 
 
-const blogPostsRouter = require('./blogPostsRouter');
+const blogRouter = require('./blogRouter');
 const app = express();
 
 
@@ -10,7 +10,7 @@ app.use(morgan('common'));
 
 // you need to import `blogPostsRouter` router and route
 // requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
-app.use('/blog-posts', blogPostsRouter);
+app.use('/blog-posts', blogRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
